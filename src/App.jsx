d64059c0f09bcd347ce882f56092a98427857de7
amanjabbas <amanjabbas4.7@gmail.com>
2026-03-19 04:11:04 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Web from "./web";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/style.css";
+function App() {
+  return (
+    <div>
+      <Web />
+    </div>
+  );
+}
+
+export default App;
